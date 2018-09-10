47634150f9536673ad3cfa756729e75f84372dc8
John Werenski <jtwerenski@gmail.com>
2018-09-10 22:05:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(num)
   while num > 0 
     puts "#{num} SECONDS!"
-    num -= num 
+    num -= 1 
   end
   return "HAPPY NEW YEAR!"
 end
@@ -12,6 +12,6 @@ def countdown_with_sleep
   while num > 0 
     sleep(5.seconds)
     puts "#{num} seconds."
-    num -= num 
+    num -= 1
   end 
 end 
